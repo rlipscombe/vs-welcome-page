ea648be505de65ba7aa773c9c75868c5bd4796f7
Roger Lipscombe <roger@differentpla.net>
2013-05-18 21:22:13 +0100

    Add stylesheet.

--- StartPage.WebApplication/HomeModule.cs
+++ StartPage.WebApplication/HomeModule.cs
@@ -9,21 +9,28 @@ namespace StartPage.WebApplication
     {
         public HomeModule()
         {
-            var rootDirectory = ConfigurationManager.AppSettings["RootDirectory"];
-            if (string.IsNullOrWhiteSpace(rootDirectory))
-                throw new ConfigurationErrorsException("RootDirectory is not configured.");
-
             Get["/"] = context =>
                 {
+                    var rootDirectory = GetRootDirectory();
                     var contentProvider = new FileContentProvider(rootDirectory);
                     var converter = new MarkdownService(contentProvider);
                     var document = converter.GetDocument("README");
-                    return document.Content;
+                    return View["Index", document];
                 };
+         
             Get["/about"] = context =>
                 {
-                    return rootDirectory;
+                    var rootDirectory = GetRootDirectory();
+                    return View["About", rootDirectory];
                 };
+        }
+
+        private static string GetRootDirectory()
+        {
+            var rootDirectory = ConfigurationManager.AppSettings["RootDirectory"];
+            if (string.IsNullOrWhiteSpace(rootDirectory))
+                throw new ConfigurationErrorsException("RootDirectory is not configured.");
+            return rootDirectory;
         }
     }
 }
