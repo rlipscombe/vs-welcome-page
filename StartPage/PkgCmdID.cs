04581b29b3dc34531f7d9ac694383610dbb42ffb
Roger Lipscombe <roger@differentpla.net>
2013-05-18 16:37:26 +0100

    Add basic VSPackage project to the solution.

---
+++ StartPage/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace RogerLipscombe.StartPage
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidViewWelcomePage =        0x100;
+
+
+    };
+}
