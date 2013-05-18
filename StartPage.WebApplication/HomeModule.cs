044a5736b94a471c309fe3b6f4441df036598813
Roger Lipscombe <roger@differentpla.net>
2013-05-18 16:27:31 +0100

    Add simple markdown rendering step.

---
+++ StartPage.WebApplication/HomeModule.cs
@@ -0,0 +1,20 @@
+﻿using Kiwi.Markdown;
+using Kiwi.Markdown.ContentProviders;
+using Nancy;
+
+namespace StartPage.WebApplication
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = context =>
+                {
+                    var contentProvider = new FileContentProvider(@"C:\Users\roger\Source\vs-start-page");
+                    var converter = new MarkdownService(contentProvider);
+                    var document = converter.GetDocument("README");
+                    return document.Content;
+                };
+        }
+    }
+}
