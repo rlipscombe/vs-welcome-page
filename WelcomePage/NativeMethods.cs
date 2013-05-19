c73e1481b060f11cf68da1fcd9ae61f9d5c0a34a
Roger Lipscombe <roger@differentpla.net>
2013-05-19 16:33:29 +0100

    Mark exception as [Serializable], split into separate files.

---
+++ WelcomePage/NativeMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace RogerLipscombe.WelcomePage
+{
+    internal static class NativeMethods
+    {
+        public static uint WM_QUIT = 0x12;
+        public static uint GW_NEXT = 2;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        internal extern static IntPtr GetTopWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        internal extern static uint GetWindowThreadProcessId(IntPtr hwnd, out uint lpdwProcessId);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal extern static bool PostMessage(HandleRef hWnd, uint message, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        internal extern static IntPtr GetWindow(IntPtr hWnd, uint uCmd);
+    }
+}
--- WelcomePage/WelcomePagePackage.cs
+++ WelcomePage/WelcomePagePackage.cs
@@ -405,31 +405,4 @@ namespace RogerLipscombe.WelcomePage
             return VSConstants.S_OK;
         }
     }
-
-    internal static class NativeMethods
-    {
-        public static uint WM_QUIT = 0x12;
-        public static uint GW_NEXT = 2;
-
-        [DllImport("user32.dll", SetLastError = true)]
-        internal extern static IntPtr GetTopWindow(IntPtr hWnd);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        internal extern static uint GetWindowThreadProcessId(IntPtr hwnd, out uint lpdwProcessId);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        internal extern static bool PostMessage(HandleRef hWnd, uint message, IntPtr wParam, IntPtr lParam);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        internal extern static IntPtr GetWindow(IntPtr hWnd, uint uCmd);
-    }
-
-    internal class WebAppInstanceConfigurationException : Exception
-    {
-        public WebAppInstanceConfigurationException(string instanceDirectory, Exception innerException)
-            : base(string.Format("Failed to configure web app instance at '{0}'.", instanceDirectory), innerException)
-        {
-        }
-    }
 }
