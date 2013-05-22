639f184230b0a4463ac8481904367be001b6f79d
Roger Lipscombe <roger@differentpla.net>
2013-05-22 21:52:16 +0100

    Disable default favicon.

---
+++ WelcomePage.WebApplication/Bootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace WelcomePage.WebApplication
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        /// <summary>
+        /// Disable default favicon by returning null.
+        /// </summary>
+        protected override byte[] FavIcon
+        {
+            get { return null; }
+        }
+    }
+}
