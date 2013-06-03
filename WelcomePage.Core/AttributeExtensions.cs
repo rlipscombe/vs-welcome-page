40ba97caddc98c8ae501b1e837e0c4964e422766
Roger Lipscombe <roger@differentpla.net>
2013-06-03 19:44:02 +0100

    Fix up VS extension.
    - Remove SNK file; Nancy isn't strong-named.
    - Tweak .NET versions.
    - Remove ZIP file asset -- it's all included in the same process now.

---
+++ WelcomePage.Core/AttributeExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+
+namespace WelcomePage.WebApplication
+{
+    internal static class AttributeExtensions
+    {
+        public static T GetCustomAttribute<T>(this Assembly assembly)
+        {
+            var attributes = assembly.GetCustomAttributes(typeof (T), inherit: true);
+            if (attributes.Length == 0)
+            {
+                throw new InvalidOperationException(
+                    string.Format("Custom attribute of type '{0}' was not found on assembly '{1}'.",
+                                  typeof (T), assembly));
+            }
+
+            if (attributes.Length > 1)
+            {
+                throw new InvalidOperationException(
+                    string.Format("Multiple instances of custom attribute of type '{0}' were found on assembly '{1}'.",
+                                  typeof (T), assembly));
+            }
+
+            return (T) attributes[0];
+        }
+    }
+}
