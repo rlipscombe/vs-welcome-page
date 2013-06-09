0ebee50e9d9c1b46e205f887f8167b7f4e931b99
Roger Lipscombe <roger@differentpla.net>
2013-06-09 14:10:36 +0100

    Bump version number.

---
+++ SolutionInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Roger Lipscombe")]
+[assembly: AssemblyProduct("WelcomePage")]
+[assembly: AssemblyCopyright("Copyright (c) 2013 Roger Lipscombe")]
+
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyInformationalVersion("1.2.0.0")]
--- WelcomePage/Properties/AssemblyInfo.cs
+++ WelcomePage/Properties/AssemblyInfo.cs
@@ -6,14 +6,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("WelcomePage")]
 [assembly: AssemblyDescription("WelcomePage Visual Studio Extension")]
-[assembly: AssemblyCompany("Roger Lipscombe")]
-[assembly: AssemblyProduct("WelcomePage")]
-[assembly: AssemblyCopyright("Copyright (c) 2013 Roger Lipscombe")]
 [assembly: ComVisible(false)]     
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
-
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
 
 [assembly: InternalsVisibleTo("WelcomePage_UnitTests")]
